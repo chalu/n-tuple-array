994f562735fd8da8924ca2692cdd67f8a002ed1d
Charles Opute Odili <chaluwa@gmail.com>
2024-03-05 13:49:51 -0700

    fix lint errors in esm example code

--- src/examples/js/basic.mjs
+++ src/examples/js/basic.mjs
@@ -1,23 +1,25 @@
 import tuplesFromArray from '../../../dist/esm/index.mjs';
 
-// some setup
+// Some setup
 const numbers = Array.from({length: 100}, (_, i) => i + 1);
-const isEven = (item) => {
-    if (
-        !item
+const isEven = item => {
+	if (
+		!item
         || typeof item !== 'number'
         || item % 2 !== 0
-    ) return false;
+	) {
+		return false;
+	}
 
-    return true;
+	return true;
 };
 
-// use the lib
+// Use the lib
 const quintetIterator = tuplesFromArray({
-    list: numbers, maxItems: 5, match: isEven
+	list: numbers, maxItems: 5, match: isEven,
 });
 
 for (const quintet of quintetIterator) {
-    // prints [ 2, 4, 6, 8, 10 ] ... [ 92, 94, 96, 98, 100 ]
+	// Prints [ 2, 4, 6, 8, 10 ] ... [ 92, 94, 96, 98, 100 ]
 	console.log(quintet);
 }
