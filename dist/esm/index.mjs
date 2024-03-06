b0f35e3fac1587589b41b4125be1e123f23ec9c7
Charles Opute Odili <chaluwa@gmail.com>
2024-03-05 21:41:34 -0700

    document what is exported

--- dist/esm/index.mjs
+++ dist/esm/index.mjs
@@ -1,3 +1,8 @@
+;
+/**
+ * An exception than can be thrown if there is no input array, `maxItems` is <= 0 or is not
+ * a number, or `match` is not a function
+ */
 export class InvalidInvocationParameterError extends Error {
 }
 const validateParametersOrThrow = (list, maxItems, match) => {
@@ -14,6 +19,33 @@ const validateParametersOrThrow = (list, maxItems, match) => {
         throw new InvalidInvocationParameterError(message);
     }
 };
+/**
+ * Returns an iterable iterator that ouputs a configured
+ * list of items when iterating over a given array
+ *
+ * @typeParam T - Type of items the input list contains
+ *
+ * @param config - An object to indicate the input array `config.list`, and set the
+ * max size of items per interation `config.maxItems`. You can also optionally specify `config.match`
+ * as a function that should return true to filter in items from the input array
+ * (or false to filter them out) when deciding what items is to be included per iteration
+ *
+ * @function
+ * @throws InvalidInvocationParameterError
+ * This exception is thrown if there is no input array, `maxItems` is <= 0 or is not
+ * a number, or `match` is not a function
+ *
+ * @returns an IterableIterator
+ *
+ * @example
+ * Here is an example that will get max of 3 items from
+ * each iteration on the returned iterable
+ * ```javascript
+ * 	const iterable = tuplesFromArray({
+ * 	  list:[], maxSize: 3, match: (itm) => !!itm
+ * 	});
+ * ```
+ */
 export const tuplesFromArray = (config) => {
     const { list, match, maxItems = 2 } = config;
     validateParametersOrThrow(list, maxItems, match);
