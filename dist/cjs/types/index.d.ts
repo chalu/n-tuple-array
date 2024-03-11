5e0e59294c9ea33f522ee62778006fbe28a0920d
Charles Opute Odili <chaluwa@gmail.com>
2024-03-11 07:45:42 -0600

    Patch release (#23)
    Use cleaner README and JS examples

--- dist/cjs/types/index.d.ts
+++ dist/cjs/types/index.d.ts
@@ -1,13 +1,53 @@
-type Item<T> = T | undefined;
-type Value<T> = Array<Item<T>>;
 export type Matcher<T> = (item: T | unknown) => boolean;
 export type TupleConfig<T> = {
+    /**
+     * The input array to use
+     */
     list: T[];
+    /**
+     * The max number of items to return from the input array, per iteration
+     * @defaultValue 2
+     */
     maxItems?: number;
+    /**
+     * When provided, a function used to determine which items in the input array
+     * are eligible to be included, per iteration
+     */
     match?: Matcher<T>;
 };
+/**
+ * An exception than can be thrown if there is no input array, `maxItems` is <= 0 or is not
+ * a number, or `match` is not a function
+ */
 export declare class InvalidInvocationParameterError extends Error {
 }
-export declare const tuplesFromArray: <T>(config: TupleConfig<T>) => Iterable<Value<T>>;
+/**
+ * Returns an iterable iterator that ouputs a configured
+ * list of items when iterating over a given array
+ *
+ * @typeParam T - Type of items the input list contains
+ *
+ * @param config - An object to indicate the input array `config.list`, and set the
+ * max size of items per interation `config.maxItems`. You can also optionally specify `config.match`
+ * as a function that should return true to filter in items from the input array
+ * (or false to filter them out) when deciding what items is to be included per iteration
+ *
+ * @function
+ * @throws InvalidInvocationParameterError
+ * This exception is thrown if there is no input array, `maxItems` is <= 0 or is not
+ * a number, or `match` is not a function
+ *
+ * @returns an IterableIterator
+ *
+ * @example
+ * Here is an example that will get max of 3 items from
+ * each iteration on the returned iterable
+ * ```javascript
+ * 	const iterable = tuplesFromArray({
+ * 	  list:[], maxSize: 3, match: (itm) => !!itm
+ * 	});
+ * ```
+ */
+export declare const tuplesFromArray: <T>(config: TupleConfig<T>) => IterableIterator<(T | undefined)[]>;
 export default tuplesFromArray;
 //# sourceMappingURL=index.d.ts.map
